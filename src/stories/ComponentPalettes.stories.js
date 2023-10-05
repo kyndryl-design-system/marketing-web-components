747d2b277024ce1975845fb2531e7332afb52302
Dennis Dolliver <dennis.dolliver@kyndryl.com>
2023-10-05 16:06:31 -0500

    fix(workflow): trigger release

--- src/stories/ComponentPalettes.stories.js
+++ src/stories/ComponentPalettes.stories.js
@@ -46,7 +46,6 @@ export const Example = {
           <div class="color-box primary-color"></div>
           <div class="color-box secondary-color"></div>
           <div class="color-box tertiary-color"></div>
-          <br />
         </div>
       </div>
     `;
