78ecebf07225f114a32afb7daf90b0cb1be3162d
Dennis Dolliver <dennis.dolliver@kyndryl.com>
2023-10-05 09:17:52 -0500

    fix(button-colors): revise implementation

--- src/stories/ComponentPalettes.stories.js
+++ src/stories/ComponentPalettes.stories.js
@@ -1,5 +1,7 @@
 import { html } from 'lit';
 import { Button } from '@kyndryl-design-system/shidoka-foundation';
+import { Link } from '@kyndryl-design-system/shidoka-foundation';
+import chevronRightIcon from '@carbon/icons/es/chevron--right/20';
 
 export default {
   title: 'Shidoka Content/Component Palettes',
@@ -44,10 +46,48 @@ export const Example = {
         <div class="component-paletted-container">
           <h3>Component</h3>
           <h4>Button</h4>
-          <kd-button kind="primary-app">Primary App button</kd-button>
-          <kd-button kind="primary-web">Primary Web button</kd-button>
-          <kd-button kind="secondary">Secondary button</kd-button>
-          <kd-button kind="tertiary">Tertiary button</kd-button>
+          <kd-button
+            kind="primary-app"
+            type="button"
+            size="medium"
+            iconposition="right"
+            description=""
+            href=""
+          >
+            Primary Button
+            <kd-icon slot="icon" .icon=${chevronRightIcon}></kd-icon>
+          </kd-button>
+          <kd-button
+            kind="secondary"
+            type="button"
+            size="medium"
+            iconposition="right"
+            description=""
+            href=""
+          >
+            Secondary Button
+            <kd-icon slot="icon" .icon=${chevronRightIcon}></kd-icon>
+          </kd-button>
+          <kd-button
+            kind="tertiary"
+            type="button"
+            size="medium"
+            iconposition="right"
+            description=""
+            href=""
+          >
+            Tertiary Button
+            <kd-icon slot="icon" .icon=${chevronRightIcon}></kd-icon>
+          </kd-button>
+          <h4>Link</h4>
+          <kd-link
+            target="_self"
+            kind="secondary"
+            href="https://www.kyndryl.com"
+          >
+            Standalone Link
+            <kd-icon slot="icon" .icon=${chevronRightIcon}>></kd-icon>
+          </kd-link>
           <h4>Accent Colors</h4>
           <div class="color-box primary-color"></div>
           <div class="color-box secondary-color"></div>
