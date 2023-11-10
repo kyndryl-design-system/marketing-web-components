7d259ceb2c5104d71cd050d066c250c523718d5b
Zak Eddington <zak.eddington@wearepop.com>
2023-11-10 13:39:38 -0800

    fix(color-palette-options): add warm red and ability to omit palettes per component

--- src/common/defs/colorPalettes.ts
+++ src/common/defs/colorPalettes.ts
@@ -1,27 +1,29 @@
 /**
  * Used to create Storybook control labels that are simple and user-friendly.
  */
-export enum COLOR_PALETTE_LABELS {
-	DEFAULT = 'default',
-	DARK_STONE = 'Darkstone',
-	SPRUCE_DARK = 'Spruce Dark',
-	SPRUCE = 'Spruce',
-	DARK_GREEN = 'Dark Green',
-	LIGHT_GREEN = 'Light Green',
-	CLOUD = 'Cloud',
-	SKYE = 'Skye',
+export const COLOR_PALETTE_LABELS = {
+	DEFAULT: 'default',
+	DARK_STONE: 'Darkstone',
+	SPRUCE_DARK: 'Spruce Dark',
+	SPRUCE: 'Spruce',
+	DARK_GREEN: 'Dark Green',
+	LIGHT_GREEN: 'Light Green',
+	WARM_RED: 'Warm Red',
+	CLOUD: 'Cloud',
+	SKYE: 'Skye',
 }
 
 /**
  * CSS classes for each color palette. Also used to create Storybook control value arrays.
  */
-export enum COLOR_PALETTE_VALUES {
-	DEFAULT = '',
-	DARK_STONE = 'kd-component-palette--darkstone',
-	SPRUCE_DARK = 'kd-component-palette--spruce-dark',
-	SPRUCE = 'kd-component-palette--spruce',
-	DARK_GREEN = 'kd-component-palette--darkgreen',
-	LIGHT_GREEN = 'kd-component-palette--lightgreen',
-	CLOUD = 'kd-component-palette--cloud',
-	SKYE = 'kd-component-palette--skye',
+export const COLOR_PALETTE_VALUES = {
+	DEFAULT: '',
+	DARK_STONE: 'kd-component-palette--darkstone',
+	SPRUCE_DARK: 'kd-component-palette--spruce-dark',
+	SPRUCE: 'kd-component-palette--spruce',
+	DARK_GREEN: 'kd-component-palette--darkgreen',
+	LIGHT_GREEN: 'kd-component-palette--lightgreen',
+	WARM_RED: 'kd-component-palette--warmred',
+	CLOUD: 'kd-component-palette--cloud',
+	SKYE: 'kd-component-palette--skye',
 }
