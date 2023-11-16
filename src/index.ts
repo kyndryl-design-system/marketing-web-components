71e3e77cf2ef9c0599b4923b9c2ef6d0a06bc9ed
Zak Eddington <zak.eddington@wearepop.com>
2023-11-16 11:41:47 -0800

    fix(stats-carousel): fix build error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Eyebrow } from './components/eyebrow';
 export { AlternatingFeatureWithImage } from './components/alternatingFeatureWithImage';
-export { StatsCarousel, StatsCarouselSlide } from './components/statsCarousel';
+export { StatsCarousel, StatsCarouselSlide } from './components/patterns/statsCarousel';
